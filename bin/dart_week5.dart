b1b0a88001c0bc34fe6add7cd17fe8b8525aa4b5
samchat2561 <samchat2561@gmail.com>
2025-11-21 10:06:11 +0700

    first commit

---
+++ bin/dart_week5.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+void add(int num1, int num2) {
+  int sum = num1 + num2;
+  print('The sum is $sum');
+}
+
+void printName() {
+  String fullName = 'Mark Zuckerberg';
+  print("My name is $fullName");
+}
+
+void main() {
+  print("Enter nums:");
+  int nums1 = int.parse(stdin.readLineSync()!);
+  int nums2 = int.parse(stdin.readLineSync()!);
+
+  printName();
+  add(nums1, nums2);
+}
